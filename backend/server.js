267c20364060b19747c3cb3f31e0b20b81c58e46
bRad1995 <bradleejwilliams@yahoo.com>
2023-12-23 11:31:22 -0600

    update user and delete user routes created

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const {PORT} = process.env;
 const express = require('express')
 const app = express();
-
 const connection = require('./models/index')
 
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`))
